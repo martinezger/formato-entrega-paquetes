86c97868fdf26e786651ec80d6dd04516408eae5
Germán Martinez <germand_m@hotmail.com>
2023-03-06 22:15:20 -0300

    formato para la entrega

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+
+    name="german_40425",
+    version="1.0",
+    description="",
+    author= "German Martinez",
+    author_email = "g@g.com",
+
+    packages= ["la_entrega"],
+
+)
